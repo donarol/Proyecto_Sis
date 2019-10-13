354718a2e1c6f09abad91696732665d80368513a
rolando <mancachirolando@gmail.com>
2019-10-12 20:30:44 -0400

    cursos nuevo

--- Angular/public/src/app/curso-nuevo/curso-nuevo.component.ts
+++ Angular/public/src/app/curso-nuevo/curso-nuevo.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Errores } from '../modelos/Errores';
+import { NgForm } from '@angular/forms';
+import { CursoService } from '../servicios/curso.service';
+import { User } from '../modelos/User';
+import { UserService } from '../servicios/user.service';
 
 @Component({
   selector: 'app-curso-nuevo',
@@ -6,10 +11,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./curso-nuevo.component.css']
 })
 export class CursoNuevoComponent implements OnInit {
-
-  constructor() { }
+  private errors:Errores[]=[];
+  private spinner:boolean;
+  private spinnerDocente:boolean;
+  private spinnerSeccion:boolean;
+  private spinnerTurno:boolean;
+  private administrador:User;
+  constructor(
+    private _curso:CursoService,
+    private _administrador:UserService
+  ) {}
 
   ngOnInit() {
+    this.setErrors();
+    this.spinner=false;
+    this.spinnerDocente=false;
+    this.spinnerSeccion=false;
+    this.spinnerTurno=false;
+    this.administrador=new User;
+    this._curso.addCurso();
   }
 
+    }else{
+      console.log("no es valido");
+    }
+  }
+  setErrors():void{
+    this.errors.push(new Errores('Error al crear el Curso'));
+    this.errors.push(new Errores('Error al ingresar el nombre'));
+    this.errors.push(new Errores('Error al ingresar el Docente'));
+    this.errors.push(new Errores('Error al ingresar la Seccion'));
+    this.errors.push(new Errores('Error al ingresar el Turno'));
+    this.errors.push(new Errores('Error al ingresar la Gestion'));
+  }
+  SelecDocente(id:string){
+    console.log("me llego ");
+    console.log(id);
+    this.spinnerDocente=true;
+    this._administrador.getUser(id).subscribe(res=>{
+      
+      console.log("mi res");
+      console.log(res);
+      this.administrador=res;
+      this.spinnerDocente=false;
+    },error=>{
+      console.log("mi error");
+      console.log(error);
+      this.spinnerDocente=false;
+    });
+
+  }
 }
